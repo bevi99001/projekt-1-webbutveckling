eaeb6c82c24ec1324933cd177443a1d44f089066
bevi99001 <54471874+bevi99001@users.noreply.github.com>
2020-01-14 08:41:12 +0100

    Update artiklar.js

--- artiklar.js
+++ artiklar.js
@@ -1,13 +1,13 @@
-$.ajax
+$.ajax // hämtar artiklar.json
 ({
     url: 'artiklar.json'
-}).done(function(res, err)
+}).done(function(res, err) // funktion som anrops när det är klart
 {
-    let artiklar = document.getElementById('artiklar');
+    let artiklar = document.getElementById('artiklar'); // hitta artiklar elementet med ID:n artiklar
 
-    for(let artikel in res.artiklar)
+    for(let artikel in res.artiklar) // loopar igenom alla artiklar ifrån artiklar.json
     {
-        artikel = res.artiklar[artikel];
-        artiklar.innerHTML += `<article><h3>${artikel.titel}</h3><a href="${artikel.källa}"><img alt="${artikel.titel}" src="${artikel.bild}"></a><p>${artikel.text}</p></article>`;
+        artikel = res.artiklar[artikel]; // förenkling 
+        artiklar.innerHTML += `<article><h3>${artikel.titel}</h3><a href="${artikel.källa}"><img alt="${artikel.titel}" src="${artikel.bild}"></a><p>${artikel.text}</p></article>`; // lägger till artiklen
     }
 });
